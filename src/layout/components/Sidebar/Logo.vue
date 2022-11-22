dde72ebf820c3b4bcf758957817407f7f7afe92a
chenli <806245830@qq.com>
2022-11-22 09:38:10 +0800

    调整logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,7 +24,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
+      //title: 'Vue Admin Template',
+      title: '中国南方电网',
       logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
   }
--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,7 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+//const title = defaultSettings.title || 'Vue Admin Template'
+const title = ''
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
